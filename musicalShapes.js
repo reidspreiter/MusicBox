a1e3afa6c47f4d24fc16cd84e879d6fd641a3fa8
reidspreiter <reidspreiter@gmail.com>
2024-05-13 16:27:22 -0500

    [FT]: Music ball and star skeletons

--- musicalShapes.js
+++ musicalShapes.js
@@ -5,27 +5,72 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 });
 
-// Initialize Tone
-function startAudio() {
-    Tone.start();
-    document.getElementById("audioButton").classList.add("hide");
-}
-
-// Clamp percentage to a given scale
-function getSliderValue(min, max, percentage) {
-    return min + Math.pow(percentage, 2) * (max - min);
-}
-
 // Initialize Kaboom
 kaboom({
     canvas: document.getElementById("homeCanvas"), 
     background: [255, 255, 255],
 });
 
-// Home page scene
-scene("flyingNotes", () => {
-    const cWidth = width() / 2;
-    const cHeight = height() / 2;
+// Manage shape select buttons
+let currShape = 1;
+function changeShape(shape) {
+    if (currShape === shape) {
+        return;
+    }
+    const header = document.getElementById("header");
+    const squareButton = document.getElementById("square");
+    const circleButton = document.getElementById("circle");
+    const starButton = document.getElementById("star");
+    squareButton.innerHTML = "&#128927;";
+    circleButton.innerHTML = "&#128901;";
+    starButton.innerHTML = "&#128949;";
+    if (shape === 1) {
+        go("musicBox");
+        squareButton.innerHTML = "&#128915;";
+        header.innerText = "Music Box";
+        currShape = shape;
+    } else if (shape === 2) {
+        go("musicBall");
+        circleButton.innerHTML = "&#128905;";
+        header.innerText = "Music Ball";
+        currShape = shape;
+    } else {
+        go("musicStar");
+        starButton.innerHTML = "&#128954;";
+        header.innerText = "Music Star";
+        currShape = shape;
+    }
+}
+
+// Initialize Tone
+function startAudio() {
+    Tone.start();
+    document.getElementById("audioButton").classList.add("hide");
+}
+
+// Align percentages to a specific scale exponentially
+function expRangeAdjust(min, max, percentage) {
+    return min + Math.pow(percentage, 2) * (max - min);
+}
+
+// Align percentages to a specific scale linearly
+function linearRangeAdjust(min, max, percentage) {
+    return min + percentage * (max - min);
+}
+
+// Restrict values to a min and max
+function clamp(value, min, max) {
+    return Math.min(max, Math.max(value, min));
+}
+
+// Center height and width values
+const cWidth = width() / 2;
+const cHeight = height() / 2;
+
+//
+// Music Box
+//
+scene("musicBox", () => {
 
     // Initialize Effects
     const reverbMin = 0.001;
@@ -143,19 +188,19 @@ scene("flyingNotes", () => {
     const lowSpawnMax = 1;
     const highSpawnMin = 0.1;
     const highSpawnMax = 2;
-    let minWait = getSliderValue(lowSpawnMin, lowSpawnMax, 0.5);
-    let maxWait = getSliderValue(highSpawnMin, highSpawnMax, 0.5)
+    let minWait = expRangeAdjust(lowSpawnMin, lowSpawnMax, 0.5);
+    let maxWait = expRangeAdjust(highSpawnMin, highSpawnMax, 0.5)
 
     const yOffsetMin = 10;
     const yOffsetMax = cHeight - 10;
-    let yOffset = getSliderValue(yOffsetMin, yOffsetMax, 0.7);
+    let yOffset = expRangeAdjust(yOffsetMin, yOffsetMax, 0.7);
 
     const lowScaleMin = 0.015;
     const lowScaleMax = 0.027;
     const highScaleMin = 0.025;
     const highScaleMax = 0.046;
-    let minScale = getSliderValue(lowScaleMin, lowScaleMax, 0.3);
-    let maxScale = getSliderValue(highScaleMin, highScaleMax, 0.3);
+    let minScale = expRangeAdjust(lowScaleMin, lowScaleMax, 0.3);
+    let maxScale = expRangeAdjust(highScaleMin, highScaleMax, 0.3);
 
     const lowRotMin = -110;
     const lowRotMax = -25;
@@ -229,46 +274,66 @@ scene("flyingNotes", () => {
         ]);
     }
 
+    // Spawn slider boxes
+    const sliderParams = [
+        {type: "rate", initPerc: 0.5},
+        {type: "freq", initPerc: 0.7},
+        {type: "verb", initPerc: 0.7},
+        {type: "delay", initPerc: 0.7},
+    ];
+    for (let i = 0; i < sliderParams.length; i++) {
+        const sliderBox = add([
+            rect(musicBoxSize / 1.5 / 8, 15),
+            color(0, 0, 0),
+            anchor("center"),
+            pos(sliderLeftEdge + sliderParams[i].initPerc * sliderRange, sliderStartY + 30 * i),
+            area(),
+            sliderDrag(),
+            "slider",
+            `${sliderParams[i].type}`
+        ]);
+        sliderBox.onHover(() => {
+            setCursor("move");
+        });
+    }
+
     // Handle slider box drag
     let curDraggin = null;
-    function drag() {
-        let offset = vec2(0);
+    function sliderDrag() {
+        let xOffset = 0;
         return {
             id: "drag",
             require: ["pos", "area"],
             pick() {
                 curDraggin = this;
-                offset = mousePos().sub(this.pos)
-                this.trigger("drag")
+                xOffset = mousePos().x - this.pos.x;
             },
             update() {
                 if (curDraggin === this) {
-                    let newPos = mousePos().sub(offset);
-                    newPos.x = Math.min(Math.max(newPos.x, sliderLeftEdge), sliderRightEdge);
-                    newPos.y = this.pos.y;
-                    const perc = (newPos.x - sliderLeftEdge) / sliderRange;
-                    this.pos = newPos;
+                    const newXPos = clamp(mousePos().x - xOffset, sliderLeftEdge, sliderRightEdge);
+                    const perc = (newXPos - sliderLeftEdge) / sliderRange;
+                    this.pos.x = newXPos;
 
                     if (this.is("rate")) {
-                        minWait = getSliderValue(lowSpawnMin, lowSpawnMax, 1 - perc);
-                        maxWait = getSliderValue(highSpawnMin, highSpawnMax, 1 - perc);
+                        minWait = expRangeAdjust(lowSpawnMin, lowSpawnMax, 1 - perc);
+                        maxWait = expRangeAdjust(highSpawnMin, highSpawnMax, 1 - perc);
                     } else if (this.is("freq")) {
-                        const newFreq = getSliderValue(filterMin, filterMax, perc);
+                        const newFreq = expRangeAdjust(filterMin, filterMax, perc);
                         filter.frequency.rampTo(newFreq, 0.1);
                         if (perc < 0.5) {
-                            minRot = getSliderValue(lowRotMin, lowRotMax, perc);
-                            maxRot = getSliderValue(highRotMin, highRotMax, perc);
+                            minRot = expRangeAdjust(lowRotMin, lowRotMax, perc);
+                            maxRot = expRangeAdjust(highRotMin, highRotMax, perc);
                         } else {
                             minRot = -25;
                             maxRot = 25;
                         }
                     } else if (this.is("verb")) {
-                        reverb.wet.value = getSliderValue(reverbMin, reverbMax, perc);
-                        minScale = getSliderValue(lowScaleMin, lowScaleMax, 1 - perc);
-                        maxScale = getSliderValue(highScaleMin, highScaleMax, 1 - perc);
+                        reverb.wet.value = expRangeAdjust(reverbMin, reverbMax, perc);
+                        minScale = expRangeAdjust(lowScaleMin, lowScaleMax, 1 - perc);
+                        maxScale = expRangeAdjust(highScaleMin, highScaleMax, 1 - perc);
                     } else {
-                        delay.wet.value = getSliderValue(delayMin, delayMax, perc);
-                        yOffset = getSliderValue(yOffsetMin, yOffsetMax, perc);
+                        delay.wet.value = expRangeAdjust(delayMin, delayMax, perc);
+                        yOffset = expRangeAdjust(yOffsetMin, yOffsetMax, perc);
                     }
                 }
             },
@@ -290,34 +355,126 @@ scene("flyingNotes", () => {
     // Drop slider on mouse release
     onMouseRelease(() => {
         if (curDraggin) {
-            curDraggin.trigger("dragEnd");
             curDraggin = null;
             setCursor("default");
         }
     });
-    
-    // Spawn slider boxes
-    const sliderParams = [
-        {type: "rate", initPerc: 0.5},
+});
+
+//
+// Music Ball
+//
+scene("musicBall", () => {
+    // Black center circle
+    const musicBallRadius = width() > height() ? width() / 16 : height() / 16;
+    add([
+        circle(musicBallRadius),
+        anchor("center"),
+        pos(center()),
+        color(0, 0, 0),
+        area(),
+        "musicBall"
+    ]);
+
+    // Knobs
+    loadSprite("knob", `./knobs/knob.png`);
+    const knobSpriteHeightpx = 605;
+    const knobWidthBasedDiameter = width() / 3 / 5;
+    const knobDiameter = width() > height() * 1.3 ? knobWidthBasedDiameter : musicBallRadius * 2;
+    const knobScaleVal = knobDiameter / knobSpriteHeightpx;
+    const knobStartx = cWidth - knobDiameter / 8 - knobDiameter - knobDiameter / 4 - knobDiameter / 2;
+    const knobStarty = height() - knobDiameter;
+
+    const knobParams = [
+        {type: "pitch", initPerc: 0},
         {type: "freq", initPerc: 0.7},
-        {type: "verb", initPerc: 0.7},
-        {type: "delay", initPerc: 0.7},
+        {type: "distortion", initPerc: 0.3},
+        {type: "bit", initPerc: 0.2},
     ];
-    for (let i = 0; i < 4; i++) {
-        const sliderBox = add([
-            rect(musicBoxSize / 1.5 / 8, 15),
-            color(0, 0, 0),
+    for (let i = 0; i < knobParams.length; i++) {
+        const knob = add([
+            sprite("knob"),
             anchor("center"),
-            pos(sliderLeftEdge + sliderParams[i].initPerc * sliderRange, sliderStartY + 30 * i),
+            pos(knobStartx + (knobDiameter + knobDiameter / 4) * i, knobStarty),
+            rotate(linearRangeAdjust(-120, 120, knobParams[i].initPerc)),
+            scale(knobScaleVal),
+            knobDrag(),
             area(),
-            drag(),
-            "slider",
-            `${sliderParams[i].type}`
+            {
+                yOffset: linearRangeAdjust(-120, 120, knobParams[i].initPerc),
+            },
+            "knob",
+            `${knobParams[i].type}`,
         ]);
-        sliderBox.onHover(() => {
+        knob.onHover(() => {
             setCursor("move");
         });
     }
+
+    // Handle knob rotation
+    let curDraggin = null;
+    const sensitivity = 3;
+    function knobDrag() {
+        let yOffset = 0;
+        return {
+            id: "drag",
+            require: ["pos", "area"],
+            pick() {
+                curDraggin = this;
+                yOffset = -(mousePos().y * sensitivity) - this.yOffset;
+            },
+            update() {
+                if (curDraggin === this) {
+                    const newAngle = clamp(-(mousePos().y * sensitivity) - yOffset, -120, 120);
+                    this.angle = newAngle;
+                    this.yOffset = newAngle;
+                }
+            },
+        }
+    }
+
+    // Grab knob to turn if hovered
+    onMousePress(() => {
+        if (!curDraggin) {
+            for (const obj of get("knob")) {
+                if (obj.isHovering()) {
+                    obj.pick();
+                    break;
+                }
+            }
+        }
+    });
+
+    // Drop knob on mouse release
+    onMouseRelease(() => {
+        if (curDraggin) {
+            curDraggin = null;
+            setCursor("default");
+        }
+    });
 });
 
-go("flyingNotes");
+//
+// Music Star
+//
+scene("musicStar", () => {
+    // Draw a 6 pointed asterisk
+    function drawStar(size, xPos, yPos) {
+        const starWeight = size / 6;
+        for (let angle = 0; angle <= 120; angle += 60) {
+            add([
+                anchor("center"),
+                rect(starWeight, size),
+                pos(xPos, yPos),
+                color(0, 0, 0),
+                rotate(angle),
+            ]);
+        }
+    }
+
+    // Black center star
+    const musicStarSize = width() > height() ? width() / 8 : height() / 8;
+    drawStar(musicStarSize, width() / 2, height() / 2);
+});
+
+go("musicBox");
